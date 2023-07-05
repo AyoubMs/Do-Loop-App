2432178e8cc182e1dc8b308b09d1a815a150ab01
AyoubMS <chahayb563@gmail.com>
2023-07-05 12:45:27 +0200

    add app

---
+++ DoLoop/Program.cs
@@ -0,0 +1,37 @@
+﻿
+bool isValidAge;
+int age;
+int testNumber = 0;
+
+//do
+//{
+//    Console.WriteLine(testNumber);
+//    testNumber += 3;
+//} while (testNumber < 10);
+
+do
+{
+    Console.Write("What is your age: ");
+    string ageText = Console.ReadLine();
+
+    isValidAge = int.TryParse(ageText, out age);
+
+    if (isValidAge == false)
+    {
+        Console.WriteLine("That was an invalid age.");
+    }
+} while (isValidAge == false);
+
+
+//Console.WriteLine($"Your age is {age}");
+
+
+//do
+//{
+
+//} while (true);
+
+//while(true)
+//{
+
+//}
